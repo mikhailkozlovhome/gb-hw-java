de4d287a7a5809c0903d9078ede56ac1a7e8b02a
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-06-13 16:19:00 +0300

    Add task 4, 5 ,8

--- hw_seminar_1/src/App.java
+++ hw_seminar_1/src/App.java
@@ -3,15 +3,103 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("TASK 1");
-        task1();
+        /*
+         * System.out.println("TASK 1");
+         * task1();
+         * 
+         * System.out.println("TASK 2");
+         * task2();
+         * 
+         * System.out.println("TASK 3");
+         * task3();
+         * 
+         * System.out.println("TASK 4");
+         * task4();
+         * 
+         * 
+         * System.out.println("TASK 5");
+         * task5();
+         */
 
-        System.out.println("TASK 2");
-        task2();
+        System.out.println("TASK 8");
+        task8();
 
-        System.out.println("TASK 3");
-        task3();
+        /**
+         * System.out.println("TASK 9");
+         * task9();
+         */
 
+    }
+
+    private static void task9() {
+        /*
+         * Записать в файл следующие данные:
+         * Name Surname Age
+         * Kate Smith 20
+         * Paul Green 25
+         * Mike Black 23
+         */
+    }
+
+    private static void task8() {
+        /*
+         * Задан массив, например, nums = [1,7,3,6,5,6].
+         * Написать программу, которая найдет индекс i для этого массива
+         * такой, что сумма элементов с индексами < i равна сумме
+         * элементов с индексами > i.
+         */
+
+        int[] arr = { 1, 7, 3, 6, 5, 6 };
+        int left_sum = 0;
+        int right_sum = 0;
+        int i = 0;
+        int j = arr.length - 1;
+        left_sum = left_sum + arr[i];
+        right_sum = right_sum + arr[j];
+
+        while (left_sum != right_sum) {
+            if (left_sum < right_sum) {
+                left_sum = left_sum + arr[i++];
+            } else {
+                right_sum = right_sum + arr[j--];
+            }
+        }
+
+        for (int itemArr : arr) {
+            System.out.printf("%d\t", itemArr);
+        }
+        System.out.println();
+       
+        System.out.printf("Index is: %d, element is: %d", i, arr[i]);
+
+    }
+
+    private static void task5() {
+        /*
+         * Во фразе "Добро пожаловать на курс по Java" переставить слова
+         * в обратном порядке.
+         */
+
+        String str = "Welcome to Java course";
+        String[] arr = str.split(" ");
+        String res_str = "";
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            res_str = res_str + arr[i];
+            if (i != 0)
+                res_str = res_str + " ";
+        }
+
+        System.out.printf("String '%s' in reverce order is: '%s'", str, res_str);
+    }
+
+    private static void task4() {
+        /*
+         * Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры
+         * могут быть заменены знаком вопроса, например 2? + ?5 = 69. Требуется
+         * восстановить выражение до верного равенства. Предложить хотя бы
+         * одно решение или сообщить, что его нет.
+         */
     }
 
     private static void task3() {
